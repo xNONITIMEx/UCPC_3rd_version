a9fefe8ab6df611950a11f60c14e12f6d8632947
lдима <liahoffdim@yandex.tu>
2023-05-29 19:19:18 +0300

    Login/logout link created

--- UCPC/main/views.py
+++ UCPC/main/views.py
@@ -1,4 +1,21 @@
 from django.shortcuts import render
+from django.views.generic import TemplateView
+
+from .models import Cards
+
 
 def index(request):
     return render(request, 'index.html')
+
+class Index(TemplateView):
+    template_name = 'index.html'
+
+    def get(self, request):
+        contex = {}
+        if request.user.is_authenticated:
+            news = Cards.objects.filter(author=request.user)
+            contex['news'] = news
+        return render(request, self.template_name, contex)
+
+
+
