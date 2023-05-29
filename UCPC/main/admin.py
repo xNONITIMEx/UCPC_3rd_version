e68542a8565620edbcd4c328dcdbc8c289e614cf
lдима <liahoffdim@yandex.tu>
2023-05-29 14:37:42 +0300

    Added tag model

--- UCPC/main/admin.py
+++ UCPC/main/admin.py
@@ -1,5 +1,6 @@
 from django.contrib import admin
 
-from .models import Cards
+from .models import Cards, Tag
 
 admin.site.register(Cards)
+admin.site.register(Tag)
--- UCPC/main/models.py
+++ UCPC/main/models.py
@@ -7,6 +7,12 @@ class Cards(models.Model):
     desc = models.TextField(max_length=250, verbose_name='Описание')
     file = models.FileField(upload_to='file', verbose_name='Файл')
     author = models.ForeignKey(User, on_delete=models.CASCADE, verbose_name='Автор')
+    tags = models.ManyToManyField('Tag', verbose_name='Теги')
+
+    def get_tags(self):
+        tag_list = self.tags.all()
+        res = [tag.title for tag in tag_list]
+        return ', '.join(res)
 
     def __str__(self):
         return self.title
@@ -14,4 +20,16 @@ class Cards(models.Model):
     class Meta:
         verbose_name = 'Карта сборки'
         verbose_name_plural = 'Карты сборок'
+
+
+class Tag(models.Model):
+    title = models.CharField(max_length=50, verbose_name='Тэг')
+    date_created = models.DateTimeField(auto_now_add='True')
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        verbose_name = 'Тэг'
+        verbose_name_plural = 'Тэги'
 
