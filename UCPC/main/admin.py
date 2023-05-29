a901a46b0da688fe842bd5a1163b98cc9e9926e5
lдима <liahoffdim@yandex.tu>
2023-05-29 14:46:41 +0300

    Searching and filters added to Tags and Cards admin model

--- UCPC/main/admin.py
+++ UCPC/main/admin.py
@@ -2,5 +2,21 @@ from django.contrib import admin
 
 from .models import Cards, Tag
 
-admin.site.register(Cards)
-admin.site.register(Tag)
+
+class TagAdmin(admin.ModelAdmin):
+    list_display = ['title', 'date_created']
+
+    class Meta:
+        model =  Tag
+
+class CardsAdmin(admin.ModelAdmin):
+    list_display = ['title', 'author', 'get_tags']
+
+    search_fields = ['title', 'desc', 'author']
+    list_filter = ['tags', 'author']
+
+    class Meta:
+        model = Cards
+
+admin.site.register(Cards, CardsAdmin)
+admin.site.register(Tag, TagAdmin)
