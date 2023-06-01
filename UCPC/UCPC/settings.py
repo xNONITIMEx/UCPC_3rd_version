596be037767c380404fe2afa2a18e4f337c636d4
lдима <liahoffdim@yandex.tu>
2023-06-01 14:05:41 +0300

    Created better-looking registration with crispy-forms

--- UCPC/UCPC/settings.py
+++ UCPC/UCPC/settings.py
@@ -34,6 +34,7 @@ INSTALLED_APPS = [
     'main',
     'user',
     'crispy_forms',
+    "crispy_bootstrap5",
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
@@ -126,3 +127,5 @@ STATICFILES_DIRS = [BASE_DIR / 'static',]
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CRISPY_TEMPLATE_PACK = 'bootstrap5'
